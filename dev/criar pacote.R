754c7eb790fb3cfc904b85026b6100858e0306af
Gustavo Mourao <gustavohen1@gmail.com>
2025-04-14 12:15:27 -0300

    \

--- dev/criar pacote.R
+++ dev/criar pacote.R
@@ -4,7 +4,10 @@ library(devtools)
 # use_package("tibble")
 # use_package("stringr")
 # use_package("dplyr")
+use_package("sf")
 # usethis::use_import_from('graphics', 'abline')
+
+# usethis::use_data_raw("limites_municipais") #cria arquivos para dados internos
 
 document()
 check()
